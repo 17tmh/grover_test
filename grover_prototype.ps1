5bace76fa24b4883cb6000542fc2b251d095ef9e
17tmh <17986@gmx.de>
2022-08-02 19:00:10 +0200

    Update scripts

--- grover_prototype.ps1
+++ grover_prototype.ps1
@@ -18,13 +18,24 @@ git checkout smart-factory-data
 wsl python3 ./scripts/generateSmartFactory.py 0.05
 
 cd ..\dlgrover\dataImport
-docker-compose up -d
+# docker-compose up -d
 
+# sleep 3
+# py dataStoreWrapper.py
+# sleep 1
+# py dataStoreWrapper.py
 
+# docker-compose down
+# rm -r -fo ~\apps
+
+#cd ..\grover\src
+# & $env:HOMEPATH\anaconda3\shell\condabin\conda-hook.ps1
+#conda activate qsharp-env
+#python service.py
 
 #sleep 3
 #docker-compose down
 
-#cd $root
-#rm -r -fo dlgrover
-#rm -r -fo data-generator
+# cd $root
+# rm -r -fo dlgrover
+# rm -r -fo data-generator
