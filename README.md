9cf015c31bf1f322463bf545233ca9131699bf73
haschkt <tobias.haschke@voith.com>
2022-08-09 10:03:33 +0200

    Update: Run all script from anaconda env (no py.exe needed)

--- README.md
+++ README.md
@@ -5,13 +5,19 @@ Powershell script for running the quantum search prototype.
 ## Set-Up
 
 - Windows 10 or 11
-- Powershell script execution must be enabled:
+- Powershell script execution must be enabled. Something like that:
   - `Set-ExecutionPolicy -ExecutionPolicy Unrestricted`
 - WSL (Windows Subsystem for Linux) with `python3`, `java`, `maven`
 - `docker`, `docker-compose` (most simple: docker-desktop for Windows)
-- `py.exe`, the Python launcher for Windows, which must start a Python 3 interpreter
-- Anaconda 3 (installed in your home directory); create this environment:
-  - `conda create -n qsharp-env -c microsoft qsharp notebook`
+- `Python`, most simple: Anaconda 3 (installed in home directory)
+  - Create environment by running (last command for populating local package cache with all required QDK components):
+
+```
+conda create -n qsharp-env -c microsoft qsharp notebook
+conda install -n qsharp-env pymongo
+conda activate qsharp-env
+python -c "import qsharp"
+```
 
 ## Run
 
--- grover_prototype.ps1
+++ grover_prototype.ps1
@@ -1,10 +1,15 @@
 $root = $PSScriptRoot
 echo "Project home directory = $root"
 
+# Enable anaconda installation for this session
+& ~\anaconda3\shell\condabin\conda-hook.ps1
+# Activate the preinstalled environment for qsharp
+conda activate qsharp-env
+
 # If there is no file "init" yet: Create it and perform initialization -> run if{...}
 # If there is a file "init" already: Initialization is already done    -> run else{...}
 cd $root
-New-Item init
+New-Item init 2> $null
 
 if($?)
 {
@@ -12,7 +17,7 @@ if($?)
     git clone https://code.dbis-pro1.fernuni-hagen.de/qcdev/dlgrover.git
     git clone https://code.dbis-pro1.fernuni-hagen.de/data-preparation/data-generator.git
 
-    # Run Python script within WSL to generate data
+    # Run script to generate data from WSL
     cd $root\data-generator
     git checkout smart-factory-data
     wsl python3 ./scripts/generateSmartFactory.py 0.05
@@ -20,34 +25,26 @@ if($?)
     # Run docker-compose to create and start MongoDB container
     cd $root\dlgrover\dataImport
     docker-compose up -d
-    # Wait until MongoDB is up and running (py.exe = Python launcher for Windows)
+    # Wait until MongoDB is up and running (could take some time)
     cd $root
-    py WaitForMongo.py
-    # Populate database (py.exe = Python launcher for Windows)
+    python WaitForMongo.py
+    # Populate database
     cd $root\dlgrover\dataImport
-    py dataStoreWrapper.py
+    python dataStoreWrapper.py
 }
 else
 {
     # Container should already be there, just re-start it
     cd $root\dlgrover\dataImport
     docker-compose start
-    # Wait until MongoDB is up and running (py.exe = Python launcher for Windows)
+    # Wait until MongoDB is up and running (could take some time)
     cd $root
-    py WaitForMongo.py
+    python WaitForMongo.py
 }
 
-# Enable anaconda installation for this session
-& ~\anaconda3\shell\condabin\conda-hook.ps1
-# Enable the preinstalled environment for qsharp (hint: needs pymongo as well!)
-conda activate qsharp-env
-
-# Call host script for quantum computation with the Python interpreter from qusharp-env
+# Call host script for quantum computation
 cd $root\dlgrover\grover\src
 python service.py
-
-# Deactivate the environment
-conda deactivate
 
 # Stop the container
 cd $root\dlgrover\dataImport
@@ -55,4 +52,7 @@ docker-compose stop
 
 # Go back home
 cd $root
+
+# Deactivate the environment
+conda deactivate
 
